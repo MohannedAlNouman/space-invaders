1f0f241aba1ff7d8163ccab89c84efbf8fba6ecf
Mohanned AlNouman <mohanned.alnouman@gmail.com>
2021-08-16 01:43:54 -0400

    working again

--- src/hooks/useCanvas.js
+++ src/hooks/useCanvas.js
@@ -11,7 +11,9 @@ const useCanvas = (draw, playerX, playerY) => {
     let animationFrameId;
 
     const render = (time) => {
-      frameCount++;
+      // console.log(time);
+      //each fram takes 16.66 ms or the game runs at 60 fps
+      // frameCount++;
       draw(context, frameCount, playerX, playerY);
       animationFrameId = window.requestAnimationFrame(render);
     };
@@ -20,7 +22,8 @@ const useCanvas = (draw, playerX, playerY) => {
     return () => {
       window.cancelAnimationFrame(animationFrameId);
     };
-  }, [draw]);
+  }, []);
+  //figure out dependencies
 
   return canvasRef;
 };
